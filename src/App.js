b93cc8d0019a029c1da2bed741ad154bba37f4e5
nourhene1705 <chamtourinourhene16@gmail.com>
2025-03-15 16:52:06 +0100

    Mise a jour de Frontand

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import Header from './components/Header';
 import Footer from './components/Footer';
+// Test commit
 
 function App() {
   return (
